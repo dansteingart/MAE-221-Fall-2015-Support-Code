23d1b0daac6ebb5aed641a2a663c9c57af68818b
Dan Steingart <dan.steingart@gmail.com>
2015-10-23 11:18:27 -0400

    Update ms_example_04_10.py

--- Chapter 4 Examples/ms_example_04_10.py
+++ Chapter 4 Examples/ms_example_04_10.py
@@ -9,44 +9,48 @@ An industrial process discharges 5.6e3 m3/min of gaseous combustion products at 
 (c) Evaluating the power developed at 0.08 USD per kW-hr, determine the value of the power, in USD/year, for 8000 hours of operation annually.
 """
 
+St = {}
 
-
+#Solve for State 1 and mdot 1
 AV_1  = 5.6e3/60# m^3/s
 T_1   = 205 + 273 #K
 P_1   = 1e5 # Pa
-St1   = stater('P',P_1,'T',T_1,'air')
-mdot_1 = AV_1/St1['V']
+St[1]   = stater('P',P_1,'T',T_1,'air')
+mdot_1 = AV_1/St[1]['V']
 
+#Solve for state 2
 T_2  = 126 + 273 #K
 P_2  = 1e5 # Pa
-St2  = stater('P',P_2,'T',T_2,'air')
+St[2]  = stater('P',P_2,'T',T_2,'air')
 mdot_2 = mdot_1
 
+#Solve for state 3
 T_3  = 38.9 + 273 # C
 P_3  = 275.3 * 1000 # Pa
-St3  = stater('P',P_3,'T',T_3,'water')
+St[3]  = stater('P',P_3,'T',T_3,'water')
 P_4  = P_3
 mdot_3  = 124.7 /60 # kg/s
 
+#Solve for state 5
 P_5  = 6.9*1000   # Pa
 x_5  = 0.93
-St5  = stater('P',P_5,'Q',x_5,'water')
+St[5]  = stater('P',P_5,'Q',x_5,'water')
 
-#A
-Wdot_turbine = mdot_1*(St1['H']-St2['H'])+mdot_3*(St3['H']-St5['H'])
+#Problem A
+Wdot_turbine = mdot_1*(St[1]['H']-St[2]['H'])+mdot_3*(St[3]['H']-St[5]['H'])
 
 print "The Turbine in generating: ", round(Wdot_turbine/1000,2), 'kW'
 
-#B
+#Problem B
 #Solve for State 4 to find T_4
-H_4 = St3['H'] + (mdot_1/mdot_3)*(St1['H']-St2['H'])
-St4 = stater('P',P_4,'H',H_4,'water')
-print "T4 = ", round(St4['T']-273,2), 'C'
+H_4 = St[3]['H'] + (mdot_1/mdot_3)*(St[1]['H']-St[2]['H'])
+St[4] = stater('P',P_4,'H',H_4,'water')
+print "T4 = ", round(St[4]['T']-273,2), 'C'
 
-#C 
+#Problem C 
 DpKWhr = 0.08
 DpY = (Wdot_turbine/1000 * 24*365) * DpKWhr 
 print "The plant is generating: ", round(DpY/1000,2), 'K$/year'
 
-
-print state_table([St1,St2,St3,St4,St5])
+#Print a nice table
+print state_table(St)
