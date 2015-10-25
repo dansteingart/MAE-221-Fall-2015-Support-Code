c1cf64e75d574f5c2ee26708b87df9406130bf2e
Dan Steingart <dan.steingart@gmail.com>
2015-10-25 10:45:24 -0400

    Added 2/5 of HW 5 Solutions

---
+++ HW 5 Solutions/set_5_problem_1.py
@@ -0,0 +1,39 @@
+from pithy import *
+
+"""One kilogram of air in a piston cylinder assembly undergoes two processes in series from an initial state where p1=0.5MPa, T1=227C
+Process 1-2: Constant temperature expansion until the volume is twice the initial volume.
+
+Process 2-3: Constant volume heating until the pressure is again 0.5 MPa.
+
+Sketch the two processes in series on a py diagram. Assuming ideal gas behavior, determine (a) the pressure at state 2, in MPa, (b) the temperature at state 3, in C, and for each of the processes (c) the work and heat transfer, each in kJ."""
+
+m = 1e3 #kg
+
+R = 8.31446
+M = 28.97 #gmol
+n = m/M
+
+p1 = 5e5 #Pa
+T1 = 227 + 273. #K
+
+cv_air = 798 #J/kg
+
+#Process 1 
+#p1v1 = p2v2
+V1 = n*R*T1/p1
+#print v1
+V2 = 2*V1
+p2 = p1*V1/(V2)
+T2 = T1
+
+#Process 2
+V3 = V2
+p3 = 5e5
+T3 = p3*V3/(n*R)
+
+
+W12 = n*R*T1 * log(V2/V1)
+Q12 = W12
+W23 = 0
+Q23 = cv_air*(T3-T2)
+whos(locals())
---
+++ HW 5 Solutions/set_5_problem_2.py
@@ -0,0 +1,79 @@
+from pithy import *
+
+"""A piston cylinder assembly contains air modeled as an ideal gas with a constant specific heat ratio, k = 1.4. The air undergoes a power cycle consisting of four processes in series:
+
+Process 1-2: Constant temperature expansion at 600 K from p1=5.5MPa to p2=0.4MPa
+
+Process 2-3: Polytropic expansion with n=k to p3=0.3MPa
+Process 3-4: Constant-pressure compression to V4=V1
+Process 4-1: Constant-volume heating
+
+Sketch the cycle on a py diagram. Determine (a) the work and heat transfer for each process, in kJ/kg, and (d) the thermal efficiency for the entire process (W/Q)
+
+http://steingart.princeton.edu:8004/mae221fall2015/problem_set_05#4
+"""
+
+
+k = 1.4 #Heat Ratio
+m_air = 28.97 #g/mol
+R = 8.31446
+R_air = R/m_air
+cv_air = R_air/(k-1)
+
+#State 1
+T1 = 600 #K
+p1 = 5e5 #Pa
+#pv = R_air T
+v1 = R_air*T1/p1
+
+#State 2 
+T2 = 600 #K, Constant Temperature Explantion
+p2 = 4e5 #Pa
+#p1v1 = p2v2
+v2 = p1*v1/p2
+
+
+#State 3, polytropic expansion pv**n = c
+p3 = 3e5 #Pa
+#p3v3^k = p2v2^k
+v3 =  (p2*v2**k/p3)**(1/k)
+#pv = R_Air T
+T3 = p3*v3/R_air
+
+#State 4, Constant-pressure compression to V4=V1
+p4 = p3
+v4 = v1 
+#v4/v3 = T3/T4
+T4 = T3*v4/v3
+
+#Process 1-2
+U12 = cv_air*(T2-T1)
+W12 = R_air * T1 * log(v2/v1)
+Q12 = U12 + W12
+
+#Process 2-3
+W23 = (p3*v3-p2*v2)/(1-k)
+U23 = cv_air*(T3-T2)
+Q23 = round(U23 + W23,2) 
+
+#Process 3-4
+W34 = p3*(v4-v3)
+U34 = cv_air*(T4-T3)
+Q34 = round(U34 + W34,2) 
+
+#Process 3-4
+W41 = (v4-v1)
+U41 = cv_air*(T1-T4)
+Q41 = round(U41 + W41,2) 
+
+
+Qin = Q12 + Q41
+Wcycle = W12+W23+W34+W41
+eta = Wcycle/Qin
+whos(locals())
+
+
+
+
+
+
