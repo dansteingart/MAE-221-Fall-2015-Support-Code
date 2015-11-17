5761acd5277b572afdabecb1cc437ae793724971
Dan Steingart <dan.steingart@gmail.com>
2015-11-17 17:31:21 -0500

    Update set_6_problem_5.py
    clarifications for which laws are being violated

--- HW 6 Solutions/set_6_problem_5.py
+++ HW 6 Solutions/set_6_problem_5.py
@@ -6,33 +6,32 @@ TC = 600.
 eta_max = 1-TC/TH
 
 def check():
-    if (1-Qc/Qh) > eta_max: return "impossible"
-    elif (1-Qc/Qh) == eta_max: return "reversible"
-    else: return "irreversible"
-
-def check2():
-    if Qh-Qc == Wcycle: check()
-    else: return "impossible"
-
+    if Qh-Qc == Wcycle:
+        if (1-Qc/Qh) > eta_max: return "impossible" #fails the second law balance
+        elif (1-Qc/Qh) == eta_max: return "reversible" #ideal process
+        else: return "irreversible" #entropy generating process
+    else: return "impossible" #fails the first law balance
 
 #Case A 
 Qc = 100.
 Qh = 500.
+Wcycle = Qh-Qc
 print "Case A is", check()
 
 #Case B
 Qc = 200.
 Wcycle = 250.
 Qh = 500.
-print "Case B is", check2()
+print "Case B is", check()
 
 #Case C
-Wc = 350.
+Wcycle = 350.
 Qc = 150.
-Qh = Qc+Wc
+Qh = Qc+Wcycle
 print "Case C is", check()
 
 #Case D
 Qc = 200.
 Qh = 500.
+Wcycle = Qh-Qc
 print "Case D is", check()
