aabd0ad2e1a5a00e558ce04ce9e16f4c8731ab4e
Dan Steingart <dan.steingart@gmail.com>
2016-01-16 15:42:41 -0500

    Update midterm_3_problem_1.py

--- Midterm 3 (Bonus) Solutions/midterm_3_problem_1.py
+++ Midterm 3 (Bonus) Solutions/midterm_3_problem_1.py
@@ -72,7 +72,8 @@ eta = 1 - (-Q_out/(Q_in_1+Q_in_2))
 W_net = W_turb_1 + W_turb_2 + W_pump
 
 print "Q_in_1 = h1-h6 = %.2f kJ/kg" % Q_in_1
-print
+print "Q_in_2 = h3-h2 = %.2f kJ/kg" % Q_in_2
+
 print "(part b) the thermal efficiency of the cycle"
 print "eta = 1 - (-Q_out/(Q_in_1+Q_in_2)) = %.2f" % eta
 print
