0ce39eb02434f0e8ace807c53e9048cd75c41deb
Dan Steingart <dan.steingart@gmail.com>
2016-01-16 21:07:00 -0500

    Update midterm_3_problem_3.py
    cleaner solution, with plots.

--- Midterm 3 (Bonus) Solutions/midterm_3_problem_3.py
+++ Midterm 3 (Bonus) Solutions/midterm_3_problem_3.py
@@ -13,117 +13,103 @@ from pithy import *
 
 # (c) Evaluate the ratio of the mean effective pressure of the Atkinson cycle to the mean effective pressure of the Otto cycle.
 
-print "Part A) compare PV and TS.  PV is already given by the relationship in the cycle, so determining Ts.  The trick is the change on limts on T and S are determined by the exhaust condition"
-print
-print "It helps to see that "
-print "eta = 1 - Qca/Qh"
-print "and this is the converse of the diesel cycle, where Q41 = H4 - H1 = Qca for the atkinson cycle"
+from pithy import *
 
-print "Other than that, apply the analysis from The examples in chapter 9"
+#Using air standard "cold" analysis, see comparison in MS 8e pg. 517 
 
 R = 8.31415 # J/(mol K)
 M_air = 28.97 # g/mol
-R_air = R/M_air
+R_air = R/M_air #J/(g K)
 k = 1.4
-cv = R_air/(k-1)
-cp = cv + R_air
-
-
-#pick some arbitrary ratio, it will be the same between the two cycles here
+cv = R_air/(k-1) #J/(g K)
+cp = cv+R_air #J/(g K)
 
 r = 8.0 #compression ratio
+TC = 300 # K
+Qin = 2000 #kJ/kg
 
-TC = 300 #K #corrected to that of problem statement
-
-
-#1 to 2, Isentropic Compression
-
-# Delta S = 0 = cv*ln(T2/T1) + R_air ln (V2/V1) 
-# Delta S = 0 = cv*ln(T2/T1) + R_air ln (1/8) 
-# 0 = cv*ln(T2/T1) + R_air ln (1/8.) 
-# cv*ln(T2/T1) = - R_air ln (1/8.)
-T1 = TC
-P1 = 1e5
-V1 = 0.001 # m^3 #picked for convinience, or you could have just done the example mass normalised
+figure(figsize=(9,9)) #Set figure size
 
 
-T2 = T1*exp(-(R_air/cv)*log(1/8.))
-P2 = P1*T2/T1 *(r)
+for c in ['o','a']:
+    if c == 'a': mp = '--'
+    else: mp = 'r-'
+
+    #Starting Conditions
+    T1 = TC
+    P1 = 1e5
+    V1 = 0.0005663369 # m^3
+    mass = P1*V1/(R_air*T1)
+    
+    #1-2 Isentropic Heating
+    T2 = T1*exp(-(R_air/cv)*log(1/r))
+    P2 = P1*T2/T1 *(r)
+    
+    V2 = mass*R_air*T2/P2
+    
+    #2 to 3, CV heating 
+    Q23 = Qin
+    T3 =  T2+(Q23/cv)
+    V3 = V2
+    P3 = mass*R_air*T3/V3
+
+    if c == 'o':
+        #3 to 4, isentropic expansion
+        V4 = V1
+        T4 = T3*exp(-(R_air/cv)*log(V4/V3))
+        P4 = (P3)*(T4/T3)**(k/(k-1))
+    
+    if c == 'a':
+        #3 to 4, isentropic expansion
+        P4 = P1
+        T4 = T3*(P4/P3)**((k-1)/k)
+        V4 = mass*R_air*T4/P4
+    
+    #V4 = V1
+    
+    #Calculate Heats/Work
+    if c == 'o': Q41 = cv*(T4-T1)
+    if c == 'a': Q41 = cp*(T4-T1)
+    
+    S1 = 0
+    S2 = S1
+    S3 = S2 + mass*(cp*log(T3/T2)-R_air*log(P3/P2))
+    S4 = S3 
+    
+    Ps = [P1,P2,P3,P4,P1]
+    Vs = [V1,V2,V3,V4,V1]
+    Ts = [T1,T2,T3,T4,T1]
+    Ss = [S1,S1,S3,S4,S1]
+    
+    eta = 1 - (Q41/Q23)
+    Wcycle = mass*(Q23-Q41)
+    mep = Wcycle/(max(Vs)-min(Vs)) 
 
 
-#2 to 3, cv heating @ Q23
-T3 = T2 + 2000/cv
-P3 = P2*T3/T2 
+    subplot(2,1,1)
+    plot(Vs,Ps,mp,label="r = %.0f, eta = %.2f, MEP =  %.1f kPa, W_cycle = %.0f kJ/kg" % (r,eta,mep/1e5,Wcycle))
+    for i in range(4):
+        st = str(i+1)
+        if st == "4": st = st+c
+        annotate(st,xy=(Vs[i],Ps[i]))
+    loglog()
 
-print "for the otto cycle"
-#3 to 4, isentropic expansion
-T4 = T3*exp(-(R_air/cv)*log(r/1.))
-P4 = P3*(T4/T3)*(1/r)
-Q41o = cv*(T4-T1)
-Q23 = cv*(T3-T2)
+    title("Comparison: Atkinson Vs. Otto")
+    ylabel("Pressure (Pa)")
+    xlabel("Volume (m^3)")
+    ylim(5e4,3e7)
+    legend(loc='best',fontsize=11,frameon=False)
+    
+    subplot(2,1,2)
+    plot(Ss,Ts,mp)
+    for i in range(4):
+        st = str(i+1)
+        if st == "4": st = st+c
+        annotate(st,xy=(Ss[i],Ts[i]))
+    xlim(-.1,1)
+    ylabel("Temperature (K)")
+    xlabel("Entropy vs. State 1 (kJ/K)")
+    semilogy()
 
-eta = 1 - (Q41o/Q23)
-
-
-
-print "Eta = %.2f" % eta
-print
-
-print "T(K),P(Pa)"
-print "%.2f,%.2e" % (T1,P1)
-print "%.2f,%.2e" % (T2,P2)
-print "%.2f,%.2e" % (T3,P3)
-print "%.2f,%.2e" % (T4,P4)
-print 
-
-
-print "Find Mean Effective Pressure"
-mass = P1*V1/(R_air*T1)
-
-Wcycle = mass*(Q23-Q41o)
-print "Wcycle = %.2f kJ" % Wcycle
-
-mep = Wcycle/(V1*(1-(1/r))) 
-
-print "mep = %.2f bar" % (mep/1e5)
-
-print 
-print "for the atkinson cycle"
-#3 to 4, isentropic expansion
-P4a = P1
-T4a = T3*exp(-(R_air/cp)*log(P3/P4a))
-
-V2 =  R_air*T2/P2
-V4a = R_air*T4/P4a
-Q41a = cp*(T4a-T1)
-Q23 = cv*(T3-T2)
-
-eta = 1 - (Q41a/Q23)
-
-
-
-print "Eta = %.2f" % eta
-print
-
-print "T(K),P(Pa)"
-print "%.2f,%.2e" % (T1,P1)
-print "%.2f,%.2e" % (T2,P2)
-print "%.2f,%.2e" % (T3,P3)
-print "%.2f,%.2e" % (T4a,P4a)
-print 
-
-
-print "Find Mean Effective Pressure"
-mass = P1*V1/(R_air*T1)
-
-Wcycle = mass*(Q23-Q41a)
-
-print "Wcycle = %.2f kJ" % Wcycle
-
-mep = Wcycle/(V4a-V1) 
-
-print "mep = %.2f bar" % (mep/1e5)
-
-
-
-
+showme()
+clf()
